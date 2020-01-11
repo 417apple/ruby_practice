e4aea729503345adb8af7a8bd8ea85042f8bd61d
鈴木優里 <lil.yf.bb7v@gmail.com>
2020-01-11 23:58:19 +0900

    gate.rb編集

--- test/gate_test.rb
+++ test/gate_test.rb
@@ -3,11 +3,12 @@ require "./lib/gate"
 require "./lib/ticket"
 
 class GateTest < Minitest::Test
-    def test_gate
+    def test_umeda_to_mikuni_when_fare_is_not_enough
         umeda = Gate.new(:umeda)
         juso = Gate.new(:juso)
+        
         ticket = Ticket.new(150)
         umeda.enter(ticket)
-        assert juso.exit(ticket)
+        refuse mikuni.exit(ticket)
     end
 end
