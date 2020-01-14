7aad7d3611bce475d969e7a808f1d93996a0ae6d
鈴木優里 <lil.yf.bb7v@gmail.com>
2020-01-15 02:35:14 +0900

    bingo.rb編集

--- lib/bingo.rb
+++ lib/bingo.rb
@@ -1,19 +1,14 @@
 class Bingo
     def self.generate_card
-    b = (1..15).to_a.sample(5)        # 1..15からランダムに5つ選んで配列を作成
-    i = (16..30).to_a.sample(5)
-    n = (31..45).to_a.sample(5)
-    n[2] = ""                         # 真ん中をfreeに
-    g = (46..60).to_a.sample(5)
-    o = (61..75).to_a.sample(5)
+    puts " B| I| N| G| O"
     
-    card = " B |  I |  N |  G |  O\n"
-    5.times do |j|
-    [b,i,n,g,o].each do |column|
-    card += column[j].to_s.rjust(2) + " | "         # 一行ずつ連結
-end
-card[-3..-1]="\n"               # 右端の' | 'を改行に置換
-end
-return card
+    [[*1..15],[*16..30],[*31..45],[*46..60],[*61..75]].map{|s|
+    s.sample(5)
+    }.transpose.each_with_index{|array, i|
+    array[2] = ' ' if i == 2
+    puts array.map{|s| s.to_s.rjust(2)}.join('|')
+    }
 end
 end
+
+Bingo.generate_card
